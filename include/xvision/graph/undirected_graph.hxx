a45039d60538fb4873b35a556bcdab18ba061e25
Constantin Pape <c.pape@gmx.net>
2019-02-23 22:05:40 +0100

    First commit

---
+++ include/xvision/graph/undirected_graph.hxx
@@ -0,0 +1,15 @@
+#pragma once
+
+
+namespace xvision {
+namespace graph {
+
+
+    template<>
+    class UndirectedGraph {
+
+    };
+
+
+}
+}
---
+++ include/xvision/segmentation/watershed.hxx
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <algorithm>
+#include <boost/pending/disjoint_sets.hpp>
+
+
+namespace xvision {
+namespace segmentation {
+
+    template<class HMAP, class SEEDS>
+    inline void seeded_watershed(const HMAP & hmap, const SEEDS & seeds) {
+
+    }
+
+
+}
+}
